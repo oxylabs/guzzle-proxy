5c79a7b8b48e6783cacb6b9fc4ee6b32ebbcdc28
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-01-09 18:10:04 +0200

    Update guzzle_datacenter.php

--- guzzle_datacenter.php
+++ guzzle_datacenter.php
@@ -1,5 +1,5 @@
 require_once 'vendor/autoload.php';
 use GuzzlesHttp\Client;
 $client = new Client();
-$r = $client->request('GET', 'https://ip.oxylabs.io', ['proxy' => 'http://192.168.2.100:60000'])
+$r = $client->request('GET', 'https://ip.oxylabs.io/location', ['proxy' => 'http://192.168.2.100:60000'])
 echo($r->getBody());
