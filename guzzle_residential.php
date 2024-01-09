f513f157378dfe760d7b2a66fff2603bc0203530
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-01-09 18:10:37 +0200

    Update guzzle_residential.php

--- guzzle_residential.php
+++ guzzle_residential.php
@@ -1,4 +1,4 @@
 require_once 'vendor/autoload.php';
 use GuzzleHttp\Client;
 $client = new Client();
-echo($client->request('GET', 'https://ip.oxylabs.io', ['proxy' => 'http://username:password@pr.oxylabs.io:7777'])->getBody());
+echo($client->request('GET', 'https://ip.oxylabs.io/location', ['proxy' => 'http://username:password@pr.oxylabs.io:7777'])->getBody());
